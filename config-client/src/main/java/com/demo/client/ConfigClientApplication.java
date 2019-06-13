a5a73a557a9a2cd732fd3d38ec06fe09c708552d
Sreemoyee Chowdhury <schowdhury@onbase.net>
2019-06-13 19:33:18 +0530

    modified file

--- config-client/src/main/java/com/demo/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/demo/client/ConfigClientApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.demo.client.config"})
+@ComponentScan(basePackages = {"com.demo.client"})
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
