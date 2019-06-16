bdd7d22433d70f830360333d872a40f2aa9b0887
Welcomesree1994@ <sreech94@gmail.com>
2019-06-16 18:04:57 +0530

    modified config-client module

--- config-client/src/main/java/com/demo/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/demo/client/ConfigClientApplication.java
@@ -1,13 +1,31 @@
 package com.demo.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.demo.client"})
+@RestController
 public class ConfigClientApplication {
+
+    @Autowired
+    private ConfigClientConfiguration properties;
+
+    @Value("${some.other.property}")
+    private String otherProperty;
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    public String printConfigProperty() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(properties.getProperty());
+        sb.append(" || ");
+        sb.append(otherProperty);
+        return sb.toString();
     }
 }
