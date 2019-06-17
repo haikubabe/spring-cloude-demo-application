6819232bbec9179d0dd6f2e2e9d38161f22a5bb5
Sreemoyee Chowdhury <schowdhury@onbase.net>
2019-06-17 19:51:58 +0530

    added new module config-client

---
+++ config-client/src/main/java/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Home Page";
+    }
+}
